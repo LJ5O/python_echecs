9fefd264d1ea5f1754dd6a994a85c0c70a3ad9e0
SaillyKévin <75009579+SaillyKevin@users.noreply.github.com>
2021-10-21 14:25:10 +0200

    Ajout sprites, début implémentation Pygame

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from pygame import *
+import pygame
+from pygame.locals import *
+import sys
+
 
 from Plateau import Plateau
 
@@ -11,6 +14,65 @@ from Roi import Roi
 
 plateau_jeu = Plateau()#Plateau, enregistre tous les tours, la position des pièces, ect
 
+pygame.init()
+#####################FONCTIONS#####################
+
+def plateauToPygame(plateau, fenetre, pygame):
+    
+    for i in range(len(plateau)):
+        for j in range(len(plateau)):
+            if(plateau[i][j]!=None):
+                pass
+                #Ajouter la pièce à l'interface
+    
+
+
+
+
+
+
+
+#####################PYGAME#####################
+
+
+#Création de la fenêtre Pygame (carré : largeur = hauteur)
+fenetre = pygame.display.set_mode((750,750), RESIZABLE)
+fond = pygame.image.load("sprites/plateau.gif").convert()
+
+#Récupération des sprites
+pionNoir = pygame.image.load("sprites/PionNoir.gif").convert_alpha()
+fouNoir = pygame.image.load("sprites/FouNoir.gif").convert_alpha()
+TourNoire = pygame.image.load("sprites/TourNoire.gif").convert_alpha()
+RoiNoir = pygame.image.load("sprites/RoiNoir.gif").convert_alpha()
+ReineNoire = pygame.image.load("sprites/ReineNoire.gif").convert_alpha()
+
+pionBlanc = pygame.image.load("sprites/PionBlanc.gif").convert_alpha()
+fouBlanc = pygame.image.load("sprites/FouBlanc.gif").convert_alpha()
+TourBlanche = pygame.image.load("sprites/TourBlanche.gif").convert_alpha()
+ReineBlanche = pygame.image.load("sprites/ReineBlanche.gif").convert_alpha()
+RoiBlanc = pygame.image.load("sprites/RoiBlanc.gif").convert_alpha()
+
+clock = pygame.time.Clock()#Je crée la clock pour actualiser l'affichage à la même vitesse sur tout ordinateur
+
+fenetre.blit(fond, (0,0))#Ajout du background
+pygame.display.flip()#Rechargement affichage
+
+#plateauToPygame(plateau_jeu.get_plateau(), fenetre, pygame)
+
+
+
+#TODO : https://coderslegacy.com/python/pygame-mouse-click/
+while 1:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+
+#####################ECHECS#####################
+            
+            
 tour_des_blancs = True#Détermine le joueur dont c'est le tour
 
 while(True):
